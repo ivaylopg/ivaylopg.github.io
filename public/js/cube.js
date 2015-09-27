0bf9eb15b96acb7efc691ea911f979d67f6fe7d0
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-09-26 22:01:01 -0700

    move cube into own .js file

--- public/js/cube.js
+++ public/js/cube.js
@@ -1,242 +1,116 @@
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+////////////////////////
+// THE CUBE
 
-// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
-
-// MIT license
-
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
-
-
-$(function(){
-
-    /* * * * 
-      This kickass css/js rotating cube is inspired by a post by Paul Hayes (http://paulrhayes.com), 
-      and uses a good amout of his code. I made some changes, specifically to the interactivity in 
+    /* * * *
+      This kickass css/js rotating cube is inspired by a post by Paul Hayes (http://paulrhayes.com),
+      and uses a good amout of his code. I made some changes, specifically to the interactivity in
       the JS, but the CSS is all his, and the idea of doing an interactive cube with CSS transforms
       intead of WebGL is his too.
      * * * */
 
-    var el = document.createElement('div'),
-        transformProps = 'transform WebkitTransform MozTransform OTransform msTransform'.split(' '),
-        transformProp = support(transformProps),
-        transitionDuration = 'transitionDuration WebkitTransitionDuration MozTransitionDuration OTransitionDuration msTransitionDuration'.split(' '),
-        transitionDurationProp = support(transitionDuration);
-
-    function support(props) {
-        for(var i = 0, l = props.length; i < l; i++) {
-            if(typeof el.style[props[i]] !== "undefined") {
-                return props[i];
-            }
-        }
-    }
-
-    var startX = -20.0;
-    var startY = 45.0;
-    var xRange = 30.0;
-    var yRange = 35.0;
-    var dur = 5000;
-    var spinSpeed = 0.2;
-
-    var viewCenter = {
+var Cube = {
+    startX: -20.0,
+    startY: 45.0,
+    xRange: 30.0,
+    yRange: 35.0,
+    spinSpeed: 0.2,
+    autoRotate: false,
+    viewport: {},
+    viewCenter: {
         x: window.innerWidth/2,
         y: $("#theBlackBox").offset().top + ($("#theBlackBox").height()/2)
     }
-    //$("#theBlackBox").offset().top - $(window).scrollTop() + ($("#theBlackBox").height()/2)
+};
 
-    Number.prototype.clamp = function(min, max) {
-        return Math.min(Math.max(this, min), max);
-    };
-
-
-    //USE A TARGET AND INCREMENTAL CHANGES/////////////////////////////////////////////////////////////////
-    var Vector2d = function(x,y) {
-        this.x = x;
-        this.y = y;
-    };
-
-    Vector2d.prototype.set = function(x,y) {
-        this.x = x;
-        this.y = y;
-    };
-
-    Vector2d.prototype.magSq =  function() {
-        var x = this.x, y = this.y;
-        return x * x + y * y;
-    };
-
-    Vector2d.prototype.mag = function(){
-        return Math.sqrt(this.magSq());
-    };
-
-    Vector2d.prototype.add = function (x, y) {
-        if (x instanceof Vector2d) {
-            this.x += x.x;
-            this.y += x.y;
-            return this;
+Cube.viewport = {
+    posVector: new Vector2d(Cube.startX, Cube.startY),
+    targVector: new Vector2d(Cube.startX, Cube.startY),
+    speed: Cube.spinSpeed,
+    el: $('.cube')[0],
+    update: function(coords) {
+        if(coords && (typeof coords.x === "number" && typeof coords.y === "number")) {
+            this.targVector.x = coords.x.clamp(Cube.startX-Cube.xRange, Cube.startX+Cube.xRange);
+            this.targVector.y = coords.y.clamp(Cube.startY-Cube.yRange, Cube.startY+Cube.yRange);
         }
-        this.x += x;
-        this.y += y;
-        return this;
-    };
-
-    Vector2d.prototype.sub = function (x, y) {
-        if (x instanceof Vector2d) {
-            this.x -= x.x;
-            this.y -= x.y;
-            return this;
-        }
-        this.x -= x;
-        this.y -= y;
-        return this;
-    };
-
-    Vector2d.prototype.div = function (n) {
-        this.x /= n;
-        this.y /= n;
-        return this;
-    };
-
-    Vector2d.prototype.mult = function (n) {
-        this.x *= n;
-        this.y *= n;
-        return this;
-    };
-
-    Vector2d.prototype.normalize = function () {
-        return this.div(this.mag());
-    };
-
-    Vector2d.prototype.setMag = function (n) {
-        return this.normalize().mult(n);
-    };
-
-
-    var mouse = {
-            start : {}
-        },
-        touch = document.ontouchmove !== undefined,
-        viewport = {
-            posVector: new Vector2d(startX, startY),
-            targVector: new Vector2d(startX, startY),
-            speed: spinSpeed,
-            el: $('.cube')[0],
-            update: function(coords) {
-                if(coords && (typeof coords.x === "number" && typeof coords.y === "number")) {
-                    this.targVector.x = coords.x.clamp(startX-xRange, startX+xRange);
-                    this.targVector.y = coords.y.clamp(startY-yRange, startY+yRange);
-                }
-                //console.log(Math.floor(coords.x),Math.floor(coords.y));
-            },
-            move: function() {
-                var dirVector = new Vector2d(this.targVector.x - this.posVector.x, this.targVector.y - this.posVector.y);
-                if (dirVector.mag() > this.speed) {
-                     dirVector.setMag(this.speed);
-                }
-                this.posVector.add(dirVector);
-                this.el.style[transformProp] = "rotateX("+this.posVector.x+"deg) rotateY("+this.posVector.y+"deg)";
-            },
-            reset: function() {
-                this.update({x: startX, y: startY});
-                delete mouse.last;
-                mouse.start.x = viewCenter.x;
-                mouse.start.y = viewCenter.y;
+        //console.log(Math.floor(coords.x),Math.floor(coords.y));
+    },
+    move: function() {
+        if (!Cube.autoRotate){
+            var dirVector = new Vector2d(this.targVector.x - this.posVector.x, this.targVector.y - this.posVector.y);
+            if (dirVector.mag() > this.speed) {
+                 dirVector.setMag(this.speed);
             }
-        };
+            this.posVector.add(dirVector);
+        } else {
+            this.posVector.y += Cube.spinSpeed/2;
+        }
 
-    viewport.duration = function() {
-        //viewport.el.style[transitionDurationProp] = dur + "ms";
-        return dur;
-    }();
 
-    var animId;
-    function draw(){
-        viewport.move();
-        animId = requestAnimationFrame(draw);
+        if (this.posVector.y >= Cube.startY+(36000)) {
+            this.posVector.y = Cube.startY;
+        }
+        this.el.style[transformProp] = "rotateX("+this.posVector.x+"deg) rotateY("+this.posVector.y+"deg)";
+    },
+    reset: function() {
+        this.update({x: Cube.startX, y: Cube.startY});
     }
-    animId = requestAnimationFrame(draw);
+};
 
-    
+Cube.resize = function(winWidth){
+    // The CSS cube stops centering correctly at widths < ~400px.
+    // I'm sure there's a better way to do this in CSS, but this
+    // way works without noticeable slowdown or overhead, so....yeah.
 
-    $(document).keydown(function(evt) {
-        switch(evt.keyCode)
-        {
-            case 27: //esc
-                viewport.reset();
-                break;
+    if (winWidth === 0) {
+        $(".cube").removeAttr("style");
+        $(".cube div").removeAttr("style");
+        $('.cube')[0].style[transformProp] = "rotateX("+Cube.viewport.posVector.x+"deg) rotateY("+Cube.viewport.posVector.y+"deg)";
+    } else {
+        var ratio = (winWidth/500);
+        ratio.clamp(0.75,1.0);
 
-            default:
-                break;
-        };
-    })
-
-    $("#theBlackBox").mouseenter(function(evt){
-        //console.log("IN");
-
-        delete mouse.last;
-        
-
-        //mouse.start.x = evt.pageX;
-        //mouse.start.y = evt.pageY;
-
-        mouse.start.x = viewCenter.x;
-        mouse.start.y = viewCenter.y;
-        // console.log(mouse.start);
-
-        ///*
-        $(document).bind('mousemove', function(event) {
-            event.preventDefault();
-            $('#theBlackBox').trigger('move-viewport', {x: event.pageX, y: event.pageY});
+        $(".cube").css({
+            "height": 400 * ratio + "px",
+            "width": 400 * ratio +"px"
         });
-        //*/
-
-    });
-
-    $("#theBlackBox").mouseleave(function(evt){
-        //console.log("OUT");
-
-        viewport.reset();
-        delete mouse.last;
-        mouse.start.x = viewCenter.x;
-        mouse.start.y = viewCenter.y;
-
-        $(document).unbind('mousemove');
-    });
-
-
-
-    $('#theBlackBox').bind('move-viewport', function(evt, movedMouse) {
-        movementScaleFactor = 4;
-
-        viewport.update({
-            x: viewport.posVector.x + parseInt((viewCenter.y - movedMouse.y)/movementScaleFactor),
-            y: viewport.posVector.y - parseInt((viewCenter.x - movedMouse.x)/movementScaleFactor)
+        $(".cube > div").css({
+            "height": 360 * ratio + "px",
+            "width": 360 * ratio +"px",
+            "padding": 20 * ratio +"px"
         });
 
-        //console.log("mouse: ", movedMouse.x,movedMouse.y, "viewCenter: ", viewCenter.x, viewCenter.y, "offset: ", $("#theBlackBox").offset().top, "scroll: ", $(window).scrollTop(), "height: ", $("#theBlackBox").height());
+        $(".cube > div:first-child")[0].style[transformProp] = "rotateX(90deg) translateZ("+200 * ratio+"px)";
+        $(".cube > div:nth-child(2)")[0].style[transformProp] = "translateZ("+200 * ratio+"px)";
+        $(".cube > div:nth-child(3)")[0].style[transformProp] = "rotateY(90deg) translateZ("+200 * ratio+"px)";
+        $(".cube > div:nth-child(4)")[0].style[transformProp] = "rotateY(180deg) translateZ("+200 * ratio+"px)";
+        $(".cube > div:nth-child(5)")[0].style[transformProp] = "rotateY(-90deg) translateZ("+200 * ratio+"px)";
+        $(".cube > div:nth-child(6)")[0].style[transformProp] = "rotateX(-90deg) rotate(180deg) translateZ("+200 * ratio+"px)";
+    }
+
+};
+
+
+$("#theBlackBox").mouseenter(function(e){
+    $(document).on('mousemove', function(event) {
+        event.preventDefault();
+        $('#theBlackBox').trigger('move-viewport', {x: event.pageX, y: event.pageY});
+    });
+});
+
+$("#theBlackBox").mouseleave(function(e){
+    Cube.viewport.reset();
+    $(document).off('mousemove');
+});
+
+
+
+$('#theBlackBox').on('move-viewport', function(e, movedMouse) {
+    var movementScaleFactor = 4;
+
+    Cube.viewport.update({
+        x: Cube.viewport.posVector.x + parseInt((Cube.viewCenter.y - movedMouse.y)/movementScaleFactor),
+        y: Cube.viewport.posVector.y - parseInt((Cube.viewCenter.x - movedMouse.x)/movementScaleFactor)
     });
 
+    //console.log("mouse: ", movedMouse.x,movedMouse.y, "viewCenter: ", viewCenter.x, viewCenter.y, "offset: ", $("#theBlackBox").offset().top, "scroll: ", $(window).scrollTop(), "height: ", $("#theBlackBox").height());
 });
--- public/js/main.js
+++ public/js/main.js
@@ -18,35 +18,40 @@ var tinyCube = false;
 
 $(document).ready(function() {
 
-    if (window.innerWidth < 500) {
-        tinyCube = true;
-        Cube.resize(window.innerWidth);
-    }
+    if ($("body").hasClass("showCube")) {
+        console.log("cube!");
 
-    if (Modernizr.touch) {
-        //console.log("touch enabled device");
-        Cube.autoRotate = true;
-        var el = $(".cube");
-        el.on("touchstart", function(e){
-            if($(e.target).is('a, iframe')) {
-                return true;
-            }
-            e.stopPropagation();
-            el.addClass("pulse");
-            setTimeout(function(){
-                el.removeClass("pulse");
-            }, 2000);
-        });
-    }
+        if (window.innerWidth < 500) {
+            tinyCube = true;
+            Cube.resize(window.innerWidth);
+        }
+
+        if (Modernizr.touch) {
+            //console.log("touch enabled device");
+            Cube.autoRotate = true;
+            var el = $(".cube");
+            el.on("touchstart", function(e){
+                if($(e.target).is('a, iframe')) {
+                    return true;
+                }
+                e.stopPropagation();
+                el.addClass("pulse");
+                setTimeout(function(){
+                    el.removeClass("pulse");
+                }, 2000);
+            });
+        }
+
+        function draw(){
+            Cube.viewport.move();
+            animId = requestAnimationFrame(draw);
+        }
+        animId = requestAnimationFrame(draw);
+
+    };
 
     $(".hiddenEmail").html(rot13rot5Encode('<n pynff="yvaxNavz" uers="znvygb:pbagnpg@vinlybtrgbi.pbz">pbagnpg@vinlybtrgbi.pbz</n>'));
     $(".hiddenPhone").html(rot13rot5Encode('<n uers="gry:+68658391837" pynff="yvaxNavz">556.865.839.1837</n>'));
-
-    function draw(){
-        Cube.viewport.move();
-        animId = requestAnimationFrame(draw);
-    }
-    animId = requestAnimationFrame(draw);
 
 });
 
@@ -67,27 +72,31 @@ $( window ).load(function() {
 
 on_resize(function() {
     //Throttled on-resize handler
-    var winWidth = window.innerWidth;
-    if (winWidth < 500) {
-        tinyCube = true;
-    } else {
-        if (tinyCube === true) {
-            tinyCube = false;
-            Cube.resize(0);
+
+    if ($("body").hasClass("showCube")) {
+        var winWidth = window.innerWidth;
+        if (winWidth < 500) {
+            tinyCube = true;
+        } else {
+            if (tinyCube === true) {
+                tinyCube = false;
+                Cube.resize(0);
+            }
+
         }
-        
+
+        if (Cube !== undefined) {
+            Cube.viewCenter = {
+                x: winWidth/2,
+                y: $("#theBlackBox").offset().top + ($("#theBlackBox").height()/2)
+            };
+
+            if (tinyCube) {
+                Cube.resize(winWidth);
+            }
+        }
     }
 
-    if (Cube !== undefined) {
-        Cube.viewCenter = {
-            x: winWidth/2,
-            y: $("#theBlackBox").offset().top + ($("#theBlackBox").height()/2)
-        };
-      
-        if (tinyCube) {
-            Cube.resize(winWidth);
-        }
-    }
 })();
 
 $( window ).resize(function() {
@@ -101,7 +110,7 @@ $(document).keydown(function(e) {
     switch(e.keyCode)
     {
         case 27: //esc
-            Cube.viewport.reset();
+            //Cube.viewport.reset();
             break;
 
         default:
@@ -109,129 +118,10 @@ $(document).keydown(function(e) {
     }
 });
 
-$("#theBlackBox").mouseenter(function(e){
-    $(document).on('mousemove', function(event) {
-        event.preventDefault();
-        $('#theBlackBox').trigger('move-viewport', {x: event.pageX, y: event.pageY});
-    });
-});
-
-$("#theBlackBox").mouseleave(function(e){
-    Cube.viewport.reset();
-    $(document).off('mousemove');
-});
-
-
-
-$('#theBlackBox').on('move-viewport', function(e, movedMouse) {
-    var movementScaleFactor = 4;
-
-    Cube.viewport.update({
-        x: Cube.viewport.posVector.x + parseInt((Cube.viewCenter.y - movedMouse.y)/movementScaleFactor),
-        y: Cube.viewport.posVector.y - parseInt((Cube.viewCenter.x - movedMouse.x)/movementScaleFactor)
-    });
-
-    //console.log("mouse: ", movedMouse.x,movedMouse.y, "viewCenter: ", viewCenter.x, viewCenter.y, "offset: ", $("#theBlackBox").offset().top, "scroll: ", $(window).scrollTop(), "height: ", $("#theBlackBox").height());
-});
-
-
 
 ////////////////////////
 // CUSTOM FUNCTIONS
 
-
-
-
-////////////////////////
-// THE CUBE
-
-    /* * * * 
-      This kickass css/js rotating cube is inspired by a post by Paul Hayes (http://paulrhayes.com), 
-      and uses a good amout of his code. I made some changes, specifically to the interactivity in 
-      the JS, but the CSS is all his, and the idea of doing an interactive cube with CSS transforms
-      intead of WebGL is his too.
-     * * * */
-
-var Cube = {
-    startX: -20.0,
-    startY: 45.0,
-    xRange: 30.0,
-    yRange: 35.0,
-    spinSpeed: 0.2,
-    autoRotate: false,
-    viewport: {},
-    viewCenter: {
-        x: window.innerWidth/2,
-        y: $("#theBlackBox").offset().top + ($("#theBlackBox").height()/2)
-    }
-};
-
-Cube.viewport = {
-    posVector: new Vector2d(Cube.startX, Cube.startY),
-    targVector: new Vector2d(Cube.startX, Cube.startY),
-    speed: Cube.spinSpeed,
-    el: $('.cube')[0],
-    update: function(coords) {
-        if(coords && (typeof coords.x === "number" && typeof coords.y === "number")) {
-            this.targVector.x = coords.x.clamp(Cube.startX-Cube.xRange, Cube.startX+Cube.xRange);
-            this.targVector.y = coords.y.clamp(Cube.startY-Cube.yRange, Cube.startY+Cube.yRange);
-        }
-        //console.log(Math.floor(coords.x),Math.floor(coords.y));
-    },
-    move: function() {
-        if (!Cube.autoRotate){
-            var dirVector = new Vector2d(this.targVector.x - this.posVector.x, this.targVector.y - this.posVector.y);
-            if (dirVector.mag() > this.speed) {
-                 dirVector.setMag(this.speed);
-            }
-            this.posVector.add(dirVector);
-        } else {
-            this.posVector.y += Cube.spinSpeed/2;
-        }
-        
-
-        if (this.posVector.y >= Cube.startY+(36000)) {
-            this.posVector.y = Cube.startY;
-        }
-        this.el.style[transformProp] = "rotateX("+this.posVector.x+"deg) rotateY("+this.posVector.y+"deg)";
-    },
-    reset: function() {
-        this.update({x: Cube.startX, y: Cube.startY});
-    }
-};
-
-Cube.resize = function(winWidth){
-    // The CSS cube stops centering correctly at widths < ~400px.
-    // I'm sure there's a better way to do this in CSS, but this 
-    // way works without noticeable slowdown or overhead, so....yeah.
-
-    if (winWidth === 0) {
-        $(".cube").removeAttr("style");
-        $(".cube div").removeAttr("style");
-        $('.cube')[0].style[transformProp] = "rotateX("+Cube.viewport.posVector.x+"deg) rotateY("+Cube.viewport.posVector.y+"deg)";
-    } else {
-        var ratio = (winWidth/500);
-        ratio.clamp(0.75,1.0);
-
-        $(".cube").css({
-            "height": 400 * ratio + "px",
-            "width": 400 * ratio +"px"
-        });
-        $(".cube > div").css({
-            "height": 360 * ratio + "px",
-            "width": 360 * ratio +"px",
-            "padding": 20 * ratio +"px"
-        });
-
-        $(".cube > div:first-child")[0].style[transformProp] = "rotateX(90deg) translateZ("+200 * ratio+"px)";
-        $(".cube > div:nth-child(2)")[0].style[transformProp] = "translateZ("+200 * ratio+"px)";
-        $(".cube > div:nth-child(3)")[0].style[transformProp] = "rotateY(90deg) translateZ("+200 * ratio+"px)";
-        $(".cube > div:nth-child(4)")[0].style[transformProp] = "rotateY(180deg) translateZ("+200 * ratio+"px)";
-        $(".cube > div:nth-child(5)")[0].style[transformProp] = "rotateY(-90deg) translateZ("+200 * ratio+"px)";
-        $(".cube > div:nth-child(6)")[0].style[transformProp] = "rotateX(-90deg) rotate(180deg) translateZ("+200 * ratio+"px)";
-    }
-    
-};
 
 
 ////////////////////////
@@ -276,20 +166,20 @@ function on_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,250);
     var vendors = ['ms', 'moz', 'webkit', 'o'];
     for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
         window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || 
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] ||
                                       window[vendors[x]+'CancelRequestAnimationFrame'];
     }
- 
+
     if (!window.requestAnimationFrame)
         window.requestAnimationFrame = function(callback, element) {
             var currTime = new Date().getTime();
             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
               timeToCall);
             lastTime = currTime + timeToCall;
             return id;
         };
- 
+
     if (!window.cancelAnimationFrame)
         window.cancelAnimationFrame = function(id) {
             clearTimeout(id);
