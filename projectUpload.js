9e9e7a6fa422e6611c36972b8bf609067c126580
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-09-26 23:41:34 -0700

    define project Schema

--- projectUpload.js
+++ projectUpload.js
@@ -1,3 +1,5 @@
+/*eslint-disable */
+
 var mongoose = require('mongoose');
 var key = process.env.KEY;
 var mdbOptions = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }}
@@ -11,8 +13,54 @@ db.on('disconnected', function () {
 });
 
 db.once('open', function (callback) {
+  console.log("Connected to MongoDB");
+});
+
+
+
+
+var projectSchema = mongoose.Schema({
+    title: String,
+    tags: [String],
+    coverImg: String,
+    thumb: String,
+    shortDescription: String,
+    longDescription: String,
+    video: [String],
+    images: [{src: String, priority: Number}],
+    imageGroups: [{src1: String, src2: String, priiority: Number}],
+    projectDate: Date,
+    updated: Date
+});
+
+projectSchema.pre('save', function(next) {
+  var currentDate = new Date();
+  this.updated = currentDate;
+  next();
+});
+
+
+/*
+var Project = mongoose.model('Project', projectSchema);
+
+var projectData = new Project({
+    title: ""
+});
+
+projectData.save(function (err, projectData) {
+  if (err) return console.error(err);
   console.log("success");
 });
+*/
+
+/*
+Project.find(function (err, projects) {
+  if (err) return console.error(err);
+  console.log(projects);
+})
+*/
+
+
 
 
 
